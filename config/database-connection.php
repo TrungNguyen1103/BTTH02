53648375e6629a8a8a8e48d03ceaf6b65d077bdf
Nguyen Thanh Trung <trungng27@gmail.com>
2023-03-11 23:29:25 +0700

    ...

--- config/database-connection.php
+++ config/database-connection.php
@@ -1,31 +1,12 @@
 <?php
-// session_start();
-// class DBConnection{
-//     private $conn=null;
+$db_host = 'localhost';
+$db_name = 'btth01_cse485';
+$db_user = 'root';
+$db_pass = '';
 
-//     public function __construct(){
-//          // B1. Kết nối DB Server
-//          try {
-//             $this->conn = new PDO('mysql:host=localhost;dbname=btth01_cse485;port=3306', 'root','');
-//         } catch (PDOException $e) {
-//             echo $e->getMessage();
-//         }
-//     }
-
-//     public function getConnection(){
-//         return $this->conn;
-//     }
-
-
-// }
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "btth01_cse485";
-
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-// Kiểm tra kết nối
-if (!$conn) {
-    die("Kết nối đến cơ sở dữ liệu thất bại: " . mysqli_connect_error());
+try {
+    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
--- views/admin/register.php
+++ views/admin/register.php
@@ -3,53 +3,49 @@
 require 'database-connection.php';               // Create PDO object
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Lấy thông tin đăng ký từ biểu mẫu và kiểm tra tính hợp lệ của chúng
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $confirm_password = $_POST['confirm_password'];
+    $user   = $_POST['txtUser'];
+    $mail   = $_POST['txtMail'];
+    $pass1  = $_POST['txtPass1'];
+    $pass2  = $_POST['txtPass2'];
 
 
-    if (empty($name) || empty($email) || empty($password) || empty($confirm_password)) {
+    if (empty($user) || empty($mail) || empty($pass1) || empty($pass2)) {
         // Nếu bất kỳ trường nào trống, hiển thị thông báo lỗi
         echo "Vui lòng điền đầy đủ thông tin đăng ký";
     }
 
-    if (mysqli_num_rows(mysqli_query($conn,"SELECT username FROM users WHERE username='$name'")) > 0){
+    if (mysqli_num_rows(mysqli_query($conn,"SELECT username FROM users WHERE username='$user' OR email='$mail'")) > 0){
         echo "Tên đăng nhập này đã có người dùng. Vui lòng chọn tên đăng nhập khác. <a href='javascript: history.go(-1)'>Trở lại</a>";
         exit;
     }
-    if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/", $email))
+    if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/", $mail))
     {
         echo "Email này không hợp lệ. Vui lòng nhập email khác. <a href='javascript: history.go(-1)'>Trở lại</a>";
         exit;
     }
           
     //Kiểm tra email đã có người dùng chưa
-    if (mysqli_num_rows(mysqli_query($conn,"SELECT email FROM users WHERE email='$email'")) > 0)
+    if (mysqli_num_rows(mysqli_query($conn,"SELECT email FROM users WHERE email='$mail'")) > 0)
     {
         echo "Email này đã có người dùng. Vui lòng chọn Email khác. <a href='javascript: history.go(-1)'>Trở lại</a>";
         exit;
     }
      else
-    if ($password !== $confirm_password) {
-        // Nếu mật khẩu không khớp, hiển thị thông báo lỗi
-        echo "Mật khẩu và xác nhận mật khẩu không khớp";
-        // $confirm_password = md5($password);
-
-    }
-
-    else 
-    {
-        // Nếu thông tin hợp lệ, cập nhật thông tin tài khoản người dùng trong cơ sở dữ liệu
-        $sql = "INSERT INTO users (id_user,username,password,email) values ('','$name', '$password', '$email') ";
-    
-        if (mysqli_query($conn, $sql)) {
-            echo "<h1>Chúc mừng bạn đã đăng ký thành công!</h1><a href='/btth02/views/admin/login.php'>Về trang chủ</a>";
-        } else {
-            echo "Lỗi khi cập nhật thông tin tài khoản: " . mysqli_error($conn);
+     {
+        // Lưu lại bản đăng kí vào CSDL
+        $pass_hash = password_hash($pass1, PASSWORD_DEFAULT);
+        $code_hash = md5(random_bytes(20));
+        $insert_sql = "INSERT INTO users (username, email, password, activation_code)
+        VALUES ('$user', '$mail', '$pass_hash', '$code_hash')";
+        if(mysqli_query($conn,$insert_sql)){
+            echo "<p style='color:green'>Đăng kí thành công, vui lòng check Email để kích hoạt tài khoản</p>";
+            echo "<p >Quay lại <href='login.php'/p>";
+            // Kích hoạt là gì?
         }
+
+        
     }
-}
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -60,18 +56,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
 	<form action="register.php" method="post">
 		<label for="name">Họ và tên</label>
-		<input type="text" id="name" name="name" required>
+		<input type="text" id="" name="txtUser" required>
 
 		<label for="email">Địa chỉ email</label>
-		<input type="email" id="email" name="email" required>
+		<input type="email" id="" name="txtMail" required>
 
 		<label for="password">Mật khẩu</label>
-		<input type="password" id="password" name="password" required>
+		<input type="password" id="" name="txtPass1" required>
 
 		<label for="confirm_password">Xác nhận mật khẩu</label>
-		<input type="password" id="confirm_password" name="confirm_password" required>
+		<input type="password" id="" name="txtPass2" required>
 
-		<input type="submit" id="POST" value="Đăng ký" >
+		<input type="submit" id="" value="Đăng ký" >
 	</form>
 </body>
 </html>
